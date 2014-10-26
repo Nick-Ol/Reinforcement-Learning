b522edadcea7622894bacfe067c65e74e23d932c
elMathos <mathurin.massias@gmail.com>
2014-10-26 22:45:19 +0100

    Correcting stupidest mistake ever

--- TP1/policy_matrices.m
+++ TP1/policy_matrices.m
@@ -4,12 +4,12 @@ dim = length(R(1,:));
 newP = zeros(dim,dim);
 for i = 1:dim
     for j = 1:dim
-        newP(i,j) = P(i, j, pi(i));
+        newP(i,j) = P(i, j, pi(i) + 1);
     end
 end
 
 newR = zeros(dim,1);
 for i = 1:dim
-    newR(i) = R(i, pi(i));
+    newR(i) = R(i, pi(i) + 1);
 end 
 end
