cc926895a6b854c956af3e8b95247602e61f1793
elMathos <mathurin.massias@gmail.com>
2014-10-26 22:42:11 +0100

    Deleting unnecessary reshaping

--- TP1/bellman.m
+++ TP1/bellman.m
@@ -1,5 +1,5 @@
 function [TV]= bellman(pi, newP, newR ,gamma, V0)
 %compute the bellman operator
 
-TV = newR + gamma* reshape(newP*V0,size(newR));
+TV = newR + gamma.* newP*V0 ;
 end
--- TP1/pol_eval_3.m
+++ TP1/pol_eval_3.m
@@ -2,7 +2,7 @@ function [V] = pol_eval_3(pi, P, R ,gamma, V0, n_it)
 %policy evaluation using bellman operator's iterations
 
 V = V0;
-[newP, newR] = policy_matrices(P, R);
+[newP, newR] = policy_matrices(P, R, pi);
 for i = 1:n_it
-    V = reshape(bellman(pi, P, R ,gamma, V), size(V0));
+    V = bellman(pi, newP, newR ,gamma, V);
 end
