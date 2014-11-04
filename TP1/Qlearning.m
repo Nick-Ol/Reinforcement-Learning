df7188d7a68f44123101446f70a41f513048b57a
elMathos <mathurin.massias@gmail.com>
2014-11-04 08:04:59 +0100

    Using demand law in Qlearning

--- TP1/Qlearning.m
+++ TP1/Qlearning.m
@@ -1,4 +1,4 @@
-function [Q] =Qlearning(n_episodes,n_it,M,K,h,c,pr,gamma)
+function [Q] =Qlearning(n_episodes,n_it,M,K,h,c,pr,gamma, D)
 
 Q = zeros(M+1,M+1);
 Q_prev = ones(M+1,M+1)*inf;
@@ -11,8 +11,7 @@ for episode = 1:n_episodes
     for i=1:n_it %no terminal state where to stop...
         actions = randperm(M+1);
         a = actions(1)-1; %random action        
-        demands = randperm(M+1);
-        d = demands(1)-1; %random demand
+        d = simu(D); %random demand
         nextstate = Nextstate(state,a,d,M);
         reward = Reward(state,a,d,M,K,h,c,pr); %one-step reward
 
