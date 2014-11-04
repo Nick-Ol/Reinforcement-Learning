4ec5b05555ff372d6264dee17efdd59488f34a75
elMathos <mathurin.massias@gmail.com>
2014-11-04 11:38:26 +0100

    Implementing UCB

---
+++ TP2/UCB.m
@@ -0,0 +1,32 @@
+function [ rew, draws ] = UCB( T, alpha, MAB )
+%TODO deal with T < length(MAB)
+Na = zeros(1, length(MAB));
+Sa = zeros(1, length(MAB));
+
+rew = zeros(1, T);
+draws = zeros(1,T);
+
+%initializing
+for t = 1:length(MAB)
+    reward = MAB{t}.play();
+    Sa(t) = Sa(t) + reward;
+    Na(t) = Na(t) + 1;
+    
+    draws(t) = t;
+    rew(t) = reward;
+end
+    
+for t = (length(MAB)+1):T  
+    Ba = (Sa./Na) +sqrt(alpha*log(t)./Na);
+    [val, idx] = max( Ba());
+    
+    reward = MAB{idx}.play();
+    Sa(idx) = Sa(idx) + reward;
+    Na(idx) = Na(idx) + 1;
+    draws(t) = idx;
+    rew(t) = reward;
+end
+
+
+end
+
