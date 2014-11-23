93c73749a55c8500cec8eac7ff12bf0c8f550066
elMathos <mathurin.massias@gmail.com>
2014-11-23 15:15:45 +0100

    Correcting Thompson

--- TP2/Thompson.m
+++ TP2/Thompson.m
@@ -8,27 +8,18 @@ obs = zeros(1, nb_arms);
 Na = zeros(1, nb_arms);
 Sa = zeros(1, nb_arms);
 
-%initializing
-for t = 1:length(MAB)
-    Sa(t) = MAB{t}.play();
-    Na(t) = 1;
-    [val, idx] = max(Sa);
-    rew(1) = val;
-    draws(1) = idx;
-end
-
-for t = 2:T
+for t = 1:T
    for i = 1:nb_arms 
        %for each arm observe theta_a from pi_a
-        obs(i) = betarnd(Sa(t) + 1, Na(t) - Sa(t) + 1);
+        obs(i) = betarnd(Sa(i) + 1, Na(i) - Sa(i) + 1);
    end
    %pick best arm
    [val, idx] = max(obs);
-   rew(t) = val;
    draws(t) = idx;
-   %update the posterior on A_t 
-   Na(draws(t-1)) = Na(draws(t-1)) + 1;
-   Sa(draws(t-1)) = Sa(draws(t-1)) + obs(draws(t-1)); 
+   rew(t) = MAB{idx}.play();
+   %update the posterior on A_i 
+   Na(idx) = Na(idx) + 1;
+   Sa(idx) = Sa(idx) + rew(t); 
    
 end
 
