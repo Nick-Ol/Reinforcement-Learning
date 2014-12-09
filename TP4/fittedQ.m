99ee001626eeca3697f612cab9f3ce12252d225f
elMathos <mathurin.massias@gmail.com>
2014-12-09 21:33:17 +0100

    Finish mplementing fittedQ

--- TP4/fittedQ.m
+++ TP4/fittedQ.m
@@ -1,24 +1,31 @@
-function [ Q ] = fittedQ( k,n, gamma )
-alphaOld = ones(1, k);
-alphaNew = ones(1, k);
+function [ Q ] = fittedQ( k,n, gamma, maxIter )
+%k: number of features
+alphaOld = zeros(k, 1);
+alphaNew = ones(k, 1);
 thetas = rand_featureQ(k);
+iter = 0;
 
-while(max(abs(alphaNew - alphaOld)) > 0.01)
+while(iter < maxIter && max(abs(alphaNew - alphaOld)) > 0.01 )
+    iter = iter + 1
     Z = zeros(n, 1);
+    X = zeros(n, k); %n observations, k features
     for i = 1:n
         s = zeros(1, 2);
         s(1) = randi([-120, 60])/100;
         s(2) = randi([-7, 7])/100;
         a = randi([-1,1]);
         [y, r] = simulator(s, a);
-        Q = createQ(alpha, thetas);
-        Z(i) = r + gamma*max(Q(y, -1), Q(y, 0), Q(y, 1));
+        Q = createQ(alphaNew, thetas);
+        Z(i) = r + gamma*max([Q(y, -1), Q(y, 0), Q(y, 1)]);
+        %filling line number i
+        for j = 1:k
+            X(i, j) = phiQ(s, a, thetas(j, :));
+        end
     end  
-%define X
-
-%update alpha
-alphaOld = alphaNew;
-alphaNew = inv(X
+    %update alpha
+    alphaOld = alphaNew;
+    alphaNew = inv(X'*X)*X'*Z;
 end
+Q = createQ(alphaNew, thetas);
 end
 
--- TP4/mainTP4.m
+++ TP4/mainTP4.m
@@ -14,5 +14,10 @@ Q=createQ(alpha,thetasQ);
 V = @(s) Q(s,0);
 plotf(V) 
 
+%% Fitted Q
+Q_fitted = fittedQ(d, 10, gamma, 100);
+V_fitted = @(s) Q_fitted(s,0);
+plotf(V_fitted) 
+
 
 
