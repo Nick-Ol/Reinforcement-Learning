a7369122847405fd4ff3945f0086755946efa37a
Nick-Ol <clement.nicolle@student.ecp.fr>
2014-12-30 01:10:13 +0100

    Revert "linUCB improved"
    This reverts commit 9b3cea7a2f214e82994808964327314d5ccb4fb6.

--- Final Project/linUCB.m
+++ Final Project/linUCB.m
@@ -1,4 +1,4 @@
-function [ rew, draws, reg, theta_estim ] = linUCB( T, alpha, MAB, theta, sigma_noise, sample )
+function [ rew, draws, max_rew ] = linUCB( T, alpha, MAB, theta )
 % every arm has to return a d-dimensional vector
 d = size(theta,1); % theta should be vertical
 K = length(MAB);
@@ -10,31 +10,26 @@ Sa = zeros(1, length(MAB));
 
 rew = zeros(1, T);
 draws = zeros(1,T);
-reg = zeros(1,T);
+max_rew = zeros(1,T);
 
 for t = 1:T
     theta_estim = A\b;
-    selected_articles_idx = randperm(K);
-    selected_articles_idx = selected_articles_idx(1:sample);
-    upper_bound = zeros(1, sample);
-    x = zeros(sample,d);
-    rewards_th = zeros(1, sample);
-    i = 1;
-    for k = selected_articles_idx
-        x(i,:) = MAB{k}.play();
-        upper_bound(i) = theta_estim'*x(i,:)' + alpha*sqrt(x(i,:)*(A\x(i,:)'));
-        rewards_th(i) = x(i,:)*theta;
-        i = i+1;
+    upper_bound = zeros(1, K);
+    x = zeros(K,d);
+    rewards_th = zeros(1, K);
+    for k = 1:K
+        x(k,:) = MAB{k}.play();
+        upper_bound(k) = theta_estim'*x(k,:)' + alpha*sqrt(x(k,:)*(A\x(k,:)'));
+        rewards_th(k) = x(k,:)*theta;
     end
-    [val, idx] = max(upper_bound); % idx = index in selected_articles_idx
-    idx_article = selected_articles_idx(idx);
+    [val, idx] = max(upper_bound);
     
-    reward = x(idx,:)*theta + mvnrnd(0,sigma_noise^2);
-    Sa(idx_article) = Sa(idx_article) + reward;
-    Na(idx_article) = Na(idx_article) + 1;
-    draws(t) = idx_article;
+    reward = x(idx,:)*theta;
+    Sa(idx) = Sa(idx) + reward;
+    Na(idx) = Na(idx) + 1;
+    draws(t) = idx;
     rew(t) = reward;
-    reg(t) = max(rewards_th) - x(idx,:)*theta;
+    max_rew(t) = max(rewards_th);
     
     A = A + x(idx,:)'*x(idx,:);
     b = b + x(idx,:)'*reward;
