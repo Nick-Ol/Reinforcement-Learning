74c4e278d8c02c2e986dbb0596f5dcca9ae61434
Nick-Ol <clement.nicolle@student.ecp.fr>
2015-01-03 00:50:27 +0100

    linUCB_multipletheta improvement

--- Final Project/linUCB_multipletheta.m
+++ Final Project/linUCB_multipletheta.m
@@ -1,12 +1,12 @@
-function [ rew, draws, reg, Na, Sa ] = linUCB_multipletheta( T, alpha, MAB, thetas, sigma_noise, nb_samples )
+function [ rew, draws, reg, Na, Sa ] = linUCB_multipletheta( T, alpha, Arms, thetas, sigma_noise, nb_samples )
 % every arm has to return a d-dimensional vector
 d = size(thetas(:,1), 1); % theta should be vertical
-K = length(MAB);
+K = length(Arms);
 A = cell(1, K);
 b = cell(1, K);
 
-Na = zeros(1, length(MAB));
-Sa = zeros(1, length(MAB));
+Na = zeros(1, length(Arms));
+Sa = zeros(1, length(Arms));
 
 rew = zeros(1, T);
 draws = zeros(1, T);
@@ -16,11 +16,9 @@ reg = zeros(1, T);
 for t = 1:K
     A{t} = eye(d);
     b{t} = zeros(d, 1);
-    x = zeros(d, nb_samples);
     rewards_th = zeros(1, nb_samples);
     for i = 1:K
-        x(:, i) = MAB{i}.play();
-        rewards_th(i) = x(:, i)'*thetas(:, i);
+        rewards_th(i) = Arms{i}'*thetas(:, i);
     end
     
     reward = rewards_th(t) + mvnrnd(0, sigma_noise^2);
@@ -31,24 +29,22 @@ for t = 1:K
     rew(t) = reward;
     reg(t) = max(rewards_th) - rewards_th(t);
     
-    A{t} = A{t} + x(:, t)*x(:, t)';
-    b{t} = b{t} + x(:, t)*reward;
+    A{t} = A{t} + Arms{t}*Arms{t}';
+    b{t} = b{t} + Arms{t}*reward;
 
 end
 
-for t = (length(MAB)+1):T
+for t = (length(Arms)+1):T
     selected_articles_idx = randperm(K);
     %nb_samples random indices in [1:K]:
     selected_articles_idx = selected_articles_idx(1:nb_samples);
     upper_bound = zeros(1, nb_samples);
-    x = zeros(d, nb_samples);
     rewards_th = zeros(1, nb_samples);
     i = 1;
 	for k = selected_articles_idx
         theta_estim = A{k}\b{k};
-        x(:, i) = MAB{k}.play();
-        upper_bound(i) = x(:, i)'*theta_estim + alpha*sqrt(x(:, i)'*inv(A{k})*x(:, i));
-        rewards_th(i) = x(:, i)'*thetas(:, i);
+        upper_bound(i) = Arms{k}'*theta_estim + alpha*sqrt(x(:, i)'*inv(A{k})*x(:, i));
+        rewards_th(i) = Arms{k}'*thetas(:, k);
         i = i+1;
 	end
     [val, idx] = max(upper_bound); % idx = index in selected_articles_idx
@@ -61,8 +57,8 @@ for t = (length(MAB)+1):T
     rew(t) = reward;
     reg(t) = max(rewards_th) - rewards_th(idx);
     
-    A{idx_article} = A{idx_article} + x(:, idx)*x(:, idx)';
-    b{idx_article} = b{idx_article} + x(:, idx)*reward;
+    A{idx_article} = A{idx_article} + Arms{idx_article}*Arms{idx_article}';
+    b{idx_article} = b{idx_article} + Arms{idx_article}*reward;
 
 end
 
