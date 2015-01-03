05e266a1ca99c968f4ed85942dcad513aa8d27f0
elMathos <mathurin.massias@gmail.com>
2015-01-03 11:39:09 +0100

    Formatting

--- Final Project/main.m
+++ Final Project/main.m
@@ -16,7 +16,7 @@ for i=1:NbArms1
     true_rewards_exp(i)= Arms{i}'*theta1;
 end
 
-[best_rew, best_arm] = max(true_rewards_exp)
+[best_rew, best_arm] = max(true_rewards_exp);
 
 %% LinUCB
 
@@ -28,9 +28,9 @@ sigma_noise = 1;
 nb_samples = 100;
 [rew_lin,draws_lin,reg_lin,theta_estim_lin, Na_lin, Sa_lin] = linUCB(T, alpha, Arms, theta1, sigma_noise, nb_samples);
 regret_lin = cumsum(reg_lin);
-[most_pulls, most_pulled_arm] = max(Na_lin) % was the best arm, the most pulled one ?
-most_pulled_arm_rew = true_rewards_exp(most_pulled_arm) % to be compared with best rew
-best_arm_pulls = Na_lin(best_arm) % best arm pulled ? times
+[most_pulls, most_pulled_arm] = max(Na_lin);% was the best arm, the most pulled one ?
+most_pulled_arm_rew = true_rewards_exp(most_pulled_arm); % to be compared with best rew
+best_arm_pulls = Na_lin(best_arm); % best arm pulled ? times
 figure;
 plot(1:T, regret_lin)
 legend('LinUCB')
@@ -38,32 +38,32 @@ legend('LinUCB')
 %% Naive = linUCB with alpha = 0
 [rew_naive,draws_naive,reg_naive,theta_estim_naive, Na_naive, Sa_naive] = linUCB(T, 0, Arms, theta1, sigma_noise, nb_samples);
 regret_naive = cumsum(reg_naive);
-[most_pulls_naive, most_pulled_arm_naive] = max(Na_naive) % was the best arm, the most pulled one ?
-most_pulled_arm_rew_naive = true_rewards_exp(most_pulled_arm_naive) % to be compared with best rew
-best_arm_pulls_naive = Na_naive(best_arm) % best arm pulled ? times
+[most_pulls_naive, most_pulled_arm_naive] = max(Na_naive); % was the best arm, the most pulled one ?
+most_pulled_arm_rew_naive = true_rewards_exp(most_pulled_arm_naive); % to be compared with best rew
+best_arm_pulls_naive = Na_naive(best_arm); % best arm pulled ? times
 figure;
 plot(1:T, regret_naive)
 legend('Naive')
 
 %% Multiple theta
-thetas = rand(d,NbArms1);
+thetas = rand(d, NbArms1);
 for k = 1:NbArms1
-    thetas(:,k) = thetas(:,k)/norm(thetas(:,k));
+    thetas(:, k) = thetas(:, k)/norm(thetas(:, k));
 end
 
 true_rewards_exp_mul = zeros(1, NbArms1);
 for i=1:NbArms1
-    true_rewards_exp_mul(i)= Arms{i}'*thetas(:,i);
+    true_rewards_exp_mul(i)= Arms{i}'*thetas(:, i);
 end
 
-[best_rew_mul, best_arm_mul] = max(true_rewards_exp_mul)
+[best_rew_mul, best_arm_mul] = max(true_rewards_exp_mul);
 
 nb_samples = 100;
 [rew_linmul,draws_linmul,reg_linmul,Na_linmul, Sa_linmul] = linUCB_multipletheta(T, alpha, Arms, thetas, sigma_noise, nb_samples);
 regret_linmul = cumsum(reg_linmul);
-[most_pulls_mul, most_pulled_arm_mul] = max(Na_linmul) % was the best arm, the most pulled one ?
-most_pulled_arm_rew_mul = true_rewards_exp_mul(most_pulled_arm_mul) % to be compared with best rew
-best_arm_pulls_mul = Na_linmul(best_arm_mul) % best arm pulled ? times
+[most_pulls_mul, most_pulled_arm_mul] = max(Na_linmul); % was the best arm, the most pulled one ?
+most_pulled_arm_rew_mul = true_rewards_exp_mul(most_pulled_arm_mul); % to be compared with best rew
+best_arm_pulls_mul = Na_linmul(best_arm_mul); % best arm pulled ? times
 figure;
 plot(1:T, regret_linmul)
 legend('LinUCB for multiple thetas')
@@ -71,9 +71,9 @@ legend('LinUCB for multiple thetas')
 %% OFUL
 [rew_oful,draws_oful,reg_oful,theta_estim_oful,Na_oful,Sa_oful] = OFUL(T, delta, Arms, theta1, sigma_noise, nb_samples, 1);
 regret_oful = cumsum(reg_oful);
-[most_pulls_oful, most_pulled_arm_oful] = max(Na_oful) % was the best arm, the most pulled one ?
-most_pulled_arm_rew_oful = true_rewards_exp(most_pulled_arm_oful) % to be compared with best rew
-best_arm_pulls_oful = Na_oful(best_arm) % best arm pulled ? times
+[most_pulls_oful, most_pulled_arm_oful] = max(Na_oful); % was the best arm, the most pulled one ?
+most_pulled_arm_rew_oful = true_rewards_exp(most_pulled_arm_oful); % to be compared with best rew
+best_arm_pulls_oful = Na_oful(best_arm); % best arm pulled ? times
 figure;
 plot(1:T, regret_oful)
 legend('OFUL')
@@ -81,9 +81,9 @@ legend('OFUL')
 %% Thompson Sampling
 [rew_thom,draws_thom,reg_thom,Na_thom,Sa_thom] = Thompson(T, delta, Arms, theta1, sigma_noise, nb_samples);
 regret_thom = cumsum(reg_thom);
-[most_pulls_thom, most_pulled_arm_thom] = max(Na_thom) % was the best arm, the most pulled one ?
-most_pulled_arm_rew_thom = true_rewards_exp(most_pulled_arm_thom) % to be compared with best rew
-best_arm_pulls_thom = Na_thom(best_arm) % best arm pulled ? times
+[most_pulls_thom, most_pulled_arm_thom] = max(Na_thom); % was the best arm, the most pulled one ?
+most_pulled_arm_rew_thom = true_rewards_exp(most_pulled_arm_thom); % to be compared with best rew
+best_arm_pulls_thom = Na_thom(best_arm); % best arm pulled ? times
 figure;
 plot(1:T,regret_thom)
 legend('Thompson Sampling')
@@ -92,7 +92,7 @@ legend('Thompson Sampling')
 figure;
 plot(1:T, regret_lin)
 plot(1:T, regret_naive)
-plot(1:T,regret_oful)
-plot(1:T,regret_thom)
+plot(1:T, regret_oful)
+plot(1:T, regret_thom)
 legend('LinUCB', 'naive', 'OFUL', 'Thompson Sampling')
 hold off
